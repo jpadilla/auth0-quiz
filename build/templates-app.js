2cf2994b09b51ea2d99fbad0f3df4ee198a54ac0
Martin Gontovnikas <martin@gon.to>
2014-05-18 16:19:03 -0700

    New info compiled

--- build/templates-app.js
+++ build/templates-app.js
@@ -3,10 +3,15 @@ angular.module('templates-app', ['home/home.tpl.html', 'quiz/finish/finish.tpl.h
 angular.module("home/home.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("home/home.tpl.html",
     "<div>\n" +
-    "  <h1>Auth0</h1>\n" +
-    "  <p>Hi and welcome to the Auth0 quiz. Do you want to test how much you know about security, authentication and authorization of your app? Then just click on start and let's start with this.</p>\n" +
+    "  <h1 id=\"logo\"><img src=\"../vendor/auth0-styleguide/img/auth0_logo_final_blue_RGB.png\" /></h1>\n" +
+    "  <div class=\"home-description\">\n" +
+    "  	\n" +
+    "	  <p class=\"\">Hi and welcome to the Auth0 quiz. Do you want to test how much you know about security, authentication and authorization of your app?</p>\n" +
+    "	  <p>Then just click on start and let's start with this.</p>\n" +
     "\n" +
-    "  <button ui-sref=\"quiz.question({number: 0})\">Start</button>\n" +
+    "  </div>\n" +
+    "\n" +
+    "  <button class=\"btn btn-lg btn-primary\" ui-sref=\"quiz.question({number: 0})\">Start</button>\n" +
     "</div>");
 }]);
 
@@ -24,21 +29,21 @@ angular.module("quiz/question/question.tpl.html", []).run(["$templateCache", fun
   $templateCache.put("quiz/question/question.tpl.html",
     "<div>\n" +
     "  <h2>{{question.question}}</h2>\n" +
-    "  <div ng-repeat=\"answer in question.answers\">\n" +
-    "	<label>\n" +
-    "		<input type=\"radio\" ng-model=\"question.response\" ng-value=\"$index\" />\n" +
+    "  <div class=\"answer\" ng-repeat=\"answer in question.answers\">\n" +
+    "	<input type=\"radio\" id=\"option_{{$index}}\" ng-model=\"question.response\" ng-value=\"$index\" />\n" +
+    "	<label for=\"option_{{$index}}\">\n" +
     "		{{answer}}\n" +
     "  	</label>\n" +
     "  </div>\n" +
-    "  <button ng-disabled=\"empty(question)\" ng-click=\"finish()\" ng-show=\"questions.length - 1 == number\">Finish</button>\n" +
-    "  <button ng-disabled=\"empty(question)\" ng-click=\"next()\" ng-hide=\"questions.length - 1 == number\">Next</button>\n" +
+    "  <button ng-disabled=\"empty(question)\" ng-click=\"finish()\" class=\"btn btn-next btn-lg\" ng-show=\"questions.length - 1 == number\">Finish</button>\n" +
+    "  <button ng-disabled=\"empty(question)\" ng-click=\"next()\" class=\"btn btn-next btn-lg\" ng-hide=\"questions.length - 1 == number\">Next</button>\n" +
     "</div>");
 }]);
 
 angular.module("quiz/quiz.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("quiz/quiz.tpl.html",
     "<div>\n" +
-    "  <h1>Quiz</h1>\n" +
+    "  <h1 id=\"logo\"><img src=\"../vendor/auth0-styleguide/img/auth0_logo_final_blue_RGB.png\" /></h1>\n" +
     "\n" +
     "  <div ui-view ></div>\n" +
     "</div>");
